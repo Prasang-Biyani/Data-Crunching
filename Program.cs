74cda2c4f16e78af5c44e20b1f31407fed3facf2
Prasang Biyani <prasang@promactinfo.com>
2018-08-10 17:25:40 +0530

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FileHelpers;
+using System.Collections;
+using System.IO;
+
+namespace DataCrunching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////ID and IP
+            var engine = new FileHelperEngine<Idip>();
+            var Idips = engine.ReadFile("files/ip_1m.tsv");
+            SortedDictionary<int, string> sortDict = new SortedDictionary<int, string>();
+
+            foreach (var idip in Idips)
+            {
+                int result = 0;
+                if (Int32.TryParse(idip.id, out result))
+                    sortDict.Add(result, idip.ip_address);
+            }
+            ICollection keys = sortDict.Keys;
+            using (StreamWriter sw = new StreamWriter("Output.tsv"))
+            {
+                foreach (var key in keys)
+                    sw.WriteLine(key + "\t" + sortDict[(int)key] + "\t");
+            }
+            Console.WriteLine(sortDict.Count());
+            var engineUser = new FileHelperEngine<User>();
+
+        }
+    }
+}
---
+++ Record.cs
@@ -0,0 +1,21 @@
+﻿using FileHelpers;
+namespace DataCrunching
+{
+    [DelimitedRecord("\t")]
+    [IgnoreFirst]
+    public class Idip
+    {
+        public string id { get; set; }
+        public string ip_address { get; set; }
+    }
+
+    [DelimitedRecord("\t")]
+    [IgnoreFirst]
+    public class User
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string hash_password { get; set; }
+    }
+}
